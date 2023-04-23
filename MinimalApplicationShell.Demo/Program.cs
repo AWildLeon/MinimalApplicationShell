46dc48734b80e5251fe97dcee087b816536dcc0f
Leon Hubrich <mail@leon-hubrich.de>
2023-04-23 04:36:22 +0200

    Version 1.0

---
+++ MinimalApplicationShell.Demo/Program.cs
@@ -0,0 +1,136 @@
+﻿namespace MinimalApplicationShell.Demo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var commands = new Dictionary<string, Action<string[]>>();
+            commands.Add("hello", (string[] args) => Console.WriteLine("Hello, world!"));
+            commands.Add("greet", (_) =>
+            {
+                Console.Write("Enter your name: ");
+                var name = Console.ReadLine();
+                Console.WriteLine($"Hello, {name}!");
+            });
+
+            //cat
+            commands.Add("cat", delegate(string[] args)
+            {
+                //Linux CAT command
+                if (args.Length == 0)
+                {
+                    Console.WriteLine("No file specified");
+                    return;
+                }
+
+                var fileName = args[0];
+                if (!File.Exists(fileName))
+                {
+                    Console.WriteLine($"File {fileName} does not exist");
+                    return;
+                }
+
+                var fileContents = File.ReadAllText(fileName);
+                Console.WriteLine(fileContents);
+
+            });
+
+            //ls
+            commands.Add("ls", delegate(string[] args)
+            {
+                var directory = Directory.GetCurrentDirectory();
+                if (args.Length > 0)
+                {
+                    directory = args[0];
+                }
+
+                var files = Directory.GetDirectories(directory).ToList();
+                files.AddRange(Directory.GetFiles(directory));
+
+                // remove the path from the file names
+                for (var i = 0; i < files.Count; i++)
+                {
+                    files[i] = Path.GetFileName(files[i]);
+                }
+
+                foreach (var file in files)
+                {
+                    Console.WriteLine(file);
+                }
+            });
+
+            //cd
+            commands.Add("cd", delegate(string[] args)
+            {
+                if (args.Length == 0)
+                {
+                    Console.WriteLine("No directory specified");
+                    return;
+                }
+                var directory = args[0];
+                if (!Directory.Exists(directory))
+                {
+                    Console.WriteLine($"Directory {directory} does not exist");
+                    return;
+                }
+                Directory.SetCurrentDirectory(directory);
+            });
+
+            //pwd
+            commands.Add("pwd", delegate(string[] args)
+            {
+                Console.WriteLine(Directory.GetCurrentDirectory());
+            });
+
+            //mkdir
+            commands.Add("mkdir", delegate(string[] args)
+            {
+                if (args.Length == 0)
+                {
+                    Console.WriteLine("No directory specified");
+                    return;
+                }
+                var directory = args[0];
+                if (Directory.Exists(directory))
+                {
+                    Console.WriteLine($"Directory {directory} already exists");
+                    return;
+                }
+                Directory.CreateDirectory(directory);
+            });
+
+            //rm
+            commands.Add("rm", delegate(string[] args)
+            {
+                if (args.Length == 0)
+                {
+                    Console.WriteLine("No file specified");
+                    return;
+                }
+                var fileName = args[0];
+                if (!File.Exists(fileName))
+                {
+                    Console.WriteLine($"File {fileName} does not exist");
+                    return;
+                }
+                File.Delete(fileName);
+            });
+
+            //echo
+            commands.Add("echo", delegate(string[] args)
+            {
+                //echo all arguments
+                foreach (var arg in args)
+                {
+                    Console.Write(arg + " ");
+                }
+
+                Console.WriteLine();
+            });
+
+            var shell = new Shell(commands);
+            shell.Run();
+
+        }
+    }
+}
---
+++ MinimalApplicationShell/Shell.cs
@@ -0,0 +1,178 @@
+﻿namespace MinimalApplicationShell
+{
+    public class Shell
+    {
+        private Dictionary<string, Action<string[]>> _commands;
+        private List<string> history = new List<string>();
+
+        public Shell(Dictionary<string, Action<string[]>> commands)
+        {
+            _commands = commands;
+        }
+
+        public void Run()
+        {
+            while (true)
+            {
+                Console.Write("> ");
+
+                var input = ReadLineWithTabCompletion(_commands);
+
+                var inputParts = input.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+                if (inputParts.Length == 0)
+                {
+                    continue;
+                }
+
+                var command = inputParts[0];
+                var arguments = inputParts.Length > 1 ? inputParts[1..] : new string[0];
+
+                if (_commands.TryGetValue(command, out var action))
+                {
+                    action(arguments);
+                }
+                else
+                {
+                    Console.WriteLine($"Unknown command: {command}");
+                }
+            }
+        }
+
+        private List<string> GetCommandSuggestions(string prefix, IEnumerable<string> commands)
+        {
+            var suggestions = new List<string>();
+            foreach (var command in commands)
+            {
+                if (command.StartsWith(prefix))
+                {
+                    suggestions.Add(command);
+                }
+            }
+            return suggestions;
+        }
+
+        private string ReadLineWithTabCompletion(Dictionary<string, Action<string[]>> commands)
+        {
+            var input = "";
+            var suggestions = new List<string>();
+            var selectedIndex = 0;
+            int historyIndex = history.Count +1;
+            string savedNewLine = "";
+            
+
+            while (true)
+            {
+                var key = Console.ReadKey(true);
+                switch (key.Key)
+                {
+                    case ConsoleKey.Tab:
+                    {
+                        if (suggestions.Count == 0)
+                        {
+                            suggestions = GetCommandSuggestions(input, commands.Keys);
+                        }
+                        if (suggestions.Count > 0)
+                        {
+                            selectedIndex = (selectedIndex + 1) % suggestions.Count;
+                            input = suggestions[selectedIndex];
+                            Console.CursorLeft = 2;
+                            Console.Write(new string(' ', Console.BufferWidth - 2));
+                            Console.CursorLeft = 2;
+                            Console.Write(input);
+                        }
+
+                        break;
+                    }
+                    case ConsoleKey.Backspace:
+                    {
+                        if (input.Length > 0)
+                        {
+                            input = input.Substring(0, input.Length - 1);
+                            Console.Write("\b \b");
+                        }
+                        suggestions.Clear();
+                        break;
+                    }
+                    case ConsoleKey.Enter:
+                    {
+                        Console.WriteLine();
+
+                        if (input.Trim().Length > 0)
+                        {
+                            history.Add(input);
+                        }
+
+                        return input;
+                    }
+                    case ConsoleKey.UpArrow:
+                    {
+                        if (historyIndex > 0)
+                        {
+                            historyIndex --;
+                        }
+
+                        //if line is empty, skip it
+                        if (historyIndex == history.Count)
+                        {
+                            historyIndex--;
+                        }
+
+                        //check if history index is in range
+                        if (historyIndex >= 0 && historyIndex < history.Count)
+                        {
+                            if (historyIndex == history.Count - 1)
+                            {
+                                savedNewLine = input;
+                            }
+                            input = history[historyIndex];
+                            Console.CursorLeft = 2;
+                            Console.Write(new string(' ', Console.BufferWidth - 2));
+                            Console.CursorLeft = 2;
+                            Console.Write(input);
+                        }
+
+                        break;
+                    }
+                    case ConsoleKey.DownArrow:
+                    {
+                        if (historyIndex < history.Count - 1)
+                        {
+                            historyIndex++;
+                            input = history[historyIndex];
+                            Console.CursorLeft = 2;
+                            Console.Write(new string(' ', Console.BufferWidth - 2));
+                            Console.CursorLeft = 2;
+                            Console.Write(input);
+                        }
+                        else if (historyIndex == history.Count - 1)
+                        {
+                            historyIndex++;
+                            input = savedNewLine;
+                            Console.CursorLeft = 2;
+                            Console.Write(new string(' ', Console.BufferWidth - 2));
+                            Console.CursorLeft = 2;
+                            Console.Write(input);
+                        }
+
+                        break;
+                    }
+                    default:
+                    {
+                        if (key.KeyChar != '\0')
+                        {
+                            input += key.KeyChar;
+                            Console.Write(key.KeyChar);
+                            suggestions.Clear();
+                        }
+                        else
+                        {
+                            suggestions.Clear();
+                        }
+
+                        break;
+                    }
+                }
+            }
+        }
+    }
+}
